a971ae842436e678149370b2a1fb215739b122b0
Kevin <wen@WENs-MBP.lan>
2025-09-20 17:22:47 -0500

    ¨mi primer commit

---
+++ proyectokevin/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import imagendeusurio from './assets/react.svg'
+
+function Header() {
+  return (
+    <>
+        <h1>HOla!</h1>
+        <img src={imagendeusurio} />
+
+    </>
+  )
+}
+
+
+export default Header
